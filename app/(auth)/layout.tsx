72d8d69b4d3d27122e6bcca89d73ef12e6d8be1f
vtquan <vtquan124@gmail.com>
2024-11-17 22:05:13 +0700

    Update UI

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,13 @@
-import React from "react";
+"use client"
 
-export default function AuthLayout({children}: {children: React.ReactNode}) {
+import React, { useLayoutEffect } from "react";
+
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+  useLayoutEffect(() => {
+      document.documentElement.classList.remove('dark')
+      document.documentElement.classList.remove('bg-background')
+  }, [])
+
   return <div className="h-[100vh] w-[100vw] flex">
     <div className="flex-1 bg-primary hidden md:block bg-[url('/images/background.png')] bg-center"/>
     <div className="max-w-[100vw] flex-1 md:max-w-[40%] p-8">
